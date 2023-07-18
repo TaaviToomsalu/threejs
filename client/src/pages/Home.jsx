c9ee7d3ae16df283f0c00377db57ba4baa6446c3
Taavi Toomsalu <queenshelob@mail.com>
2023-07-18 19:59:08 +0300

    Finishing Home page, imports for Customizer and creating component files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { useSnapshot } from 'valtio'
 
 import state from '../store'
-
+import { CustomButton } from '../components'
 import { 
   headContainerAnimation,
   headContentAnimation,
@@ -39,7 +39,12 @@ const Home = () => {
                 Create your unique and exclusive shirt with our brand-new 3D customization tool. 
                 <strong> Unleash your imagination</strong>{' '} and define your own style.
               </p>
-              
+              <CustomButton
+                type='filled'
+                title='Customize it'
+                handleClick={() => state.intro = false}
+                customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+              />
             </motion.div>
           </motion.div>
         </motion.section>
